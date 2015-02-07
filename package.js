1a6b7f95e14bb147d2bb24197e587905570a58d1
leonzhang1109 <leonzhang1109@hotmail.com>
2015-02-06 17:32:40 -0800

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Wechat account login for meteor",
-  "version": "0.2.4",
+  "version": "0.2.5",
   "git": "https://github.com/leonzhang1109/meteor-accounts-wechat",
   "name": "leonzhang1109:accounts-wechat"
 });
